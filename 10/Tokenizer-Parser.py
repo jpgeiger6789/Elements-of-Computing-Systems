d9475cae000088ac89775b4787febbb336558be3
Jack Geiger <jack.geiger@buschusa.com>
2017-11-22 16:08:21 -0500

    update

--- 10/Tokenizer-Parser.py
+++ 10/Tokenizer-Parser.py
@@ -1,12 +1,18 @@
 #https://docs.python.org/3/howto/argparse.html
 
 import sys
-import re
+import regex #need to get from pypi - from command line: pip install regex
 import os
 
 class tokens():
     #define regular expression patterns
     def __init__(self):
+        #find comments in the Jack language
+        # the .*? matches as few characters as possible - try to find other matches
+        self.multilineCommentStart = r"(?P<multilineCommentStart>" + regex.escape(r"/*") + r".*?)"
+        self.multilineCommentEnd = r"(?P<multilineCommentEnd>" + regex.escape(r"*/") + r".*?)"
+        self.commentUntilEndofLine = r"(?P<commentUntilEndofLine>(" + regex.escape(r"//") + "|" + regex.escape(r"/*") + ".*?" + regex.escape("*/") + r").*$)"
+
         #match any of the keywords in the Jack language
         self.keyword = (r"(?P<keyword>(class)|(constructor)|(function)|(method)|(field)|(static)|(var)" +
         r"|(int)|(char)|(boolean)|(void)|(true)|(false)|(null)|(this)|(let)|(do)|(if)|(else)|(while)|(return)(?=\s|$))")
@@ -15,7 +21,7 @@ class tokens():
         the symbol regular expression matches the following symbols:
         { } ( ) [ ] . , ; + - * / & | < > = ~
         """
-        self.symbol = (r"(?P<symbol>" + "|".join(map(re.escape, r"{}()[].,;+-*/&|<>=~")) + "(?=\s|$))")
+        self.symbol = (r"(?P<symbol>" + "|".join(map(regex.escape, r"{}()[].,;+-*/&|<>=~")) + "(?=\s|$))")
 
         """
         the integerConstant regular expression matches any number in the range 0..32767.  I have allowed any number
@@ -37,8 +43,11 @@ class tokens():
 
         self.identifier = r"(?P<identifier>[a-zA-Z_][\w_]*(?=\s|$))"
 
-        self.LexicalElement = r"(?P<LexicalElement>" + r"|".join([self.keyword, self.symbol, self.integerConstant, self.StringConstant, self.identifier]) + r")"
-        self.line = r"^(?P<line>\s*(" + self.LexicalElement + "\s*)*)$"
+        self.LexicalElement = r"(" + r"|".join([self.keyword, self.symbol, self.integerConstant, self.StringConstant, self.identifier]) + r")"
+
+        #the double question mark makes a regex not match if possible
+        self.line = (r"^(?P<line>" + self.multilineCommentEnd + "??\s*(" + self.LexicalElement + "\s*)*(" +
+                     self.multilineCommentStart + "|" + self.commentUntilEndofLine +")?)$")
 
 if __debug__:
     d = tokens()
@@ -48,98 +57,85 @@ if __debug__:
     keywords = ("class|constructor|function|method|field|static|var" +
         "|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return").split("|")
     for i in keywords:
-        assert re.match(d.keyword, i)
-        assert re.match(d.LexicalElement, i)
+        assert regex.match(d.keyword, i)
+        assert regex.match(d.LexicalElement, i)
 
     #check symbols
     for i in r"{}()[].,;+-*/&|<>=~":
-        assert re.match(d.symbol, i)
-        assert re.match(d.LexicalElement, i)
+        assert regex.match(d.symbol, i)
+        assert regex.match(d.LexicalElement, i)
 
     #check integer constants
     for i in "0|01|10|0123|123|01234|1234|12345|22345|31345|32345|32745|32765|032765|32767".split("|"):
-        assert re.match(d.integerConstant, i)
-        assert re.match(d.LexicalElement, i)
+        assert regex.match(d.integerConstant, i)
+        assert regex.match(d.LexicalElement, i)
     for i in r"-1|32768|32771|32811|33111|40000|032768|032771|032811|033111|040000|3.4|3.0".split("|"):
-        assert not re.match(d.integerConstant, i)
+        assert not regex.match(d.integerConstant, i)
 
     #check string constants
     import itertools #need to chain two rainges together
     for i in itertools.chain(range(10), range(11, 32768)): #ignore newline character - Jack language doesn't support it
-        assert re.match(d.StringConstant, '"' + re.escape(chr(i)) + '"')
-        assert re.match(d.LexicalElement, '"' + re.escape(chr(i)) + '"')
-    assert re.match(d.StringConstant, '""')
-    assert re.match(d.LexicalElement, '""')
-    assert not re.match(d.StringConstant, 'abc')
-    assert  not re.match(d.StringConstant, '')
+        assert regex.match(d.StringConstant, '"' + regex.escape(chr(i)) + '"')
+        assert regex.match(d.LexicalElement, '"' + regex.escape(chr(i)) + '"')
+    assert regex.match(d.StringConstant, '""')
+    assert regex.match(d.LexicalElement, '""')
+    assert not regex.match(d.StringConstant, 'abc')
+    assert  not regex.match(d.StringConstant, '')
 
     #check lines
-    for i in [r'  class foo 12345', r'(', r'"howdy" if 00000012 let "let" ; ( ) ']:
-        assert re.match(d.line, i)
+    for line in [r'  class foo 12345 + 23456', r'( //this is a comment', r'"howdy" if 00000012 let "let" ; ( ) /* another comment*/']:
+        x = regex.match(d.line, line)
+
+        #https://stackoverflow.com/questions/28856238/how-to-get-group-name-of-match-regular-expression-in-python
+        lookupDict = [([n for n, v in a.groupdict().items() if v][0], a.group()) for a in regex.finditer(d.LexicalElement, line)]
+
+        print(x.captures())
+        print(line)
+        print(len(x.groups))
+        assert regex.match(d.line, line)
 
 class parser():
     def __init__(self, fname):
         extens = os.path.basename(fname).rsplit(".", 1)[1]
+        self.iname = fname
         self.oname = fname[:-len(extens)-1] + ".xml"
         self.tokens = tokens()
-        regEx = re.compile(self.tokens.comment)
-        #open the file, remove all whitespace and comments, return as a list
-        with open(fname, 'r') as file:
-            #generator comprehension inside a list comprehension -
-            #   remove all comments and whitespaces from each line in the input file
-            #   add all items which still exist to the list
-            self.lns = [j for j in (regEx.sub("","".join(i.split())) for i in file) if j]
-            regEx = re.compile(self.tokens.placeHolder)
-            i = 0
-            while i < len(self.lns):
-                match = regEx.match(self.lns[i])
-                if match:
-                    p = match.group("placeHolder")
-                    self.tokens.symbolDict[p] = i
-                    self.lns.pop(i)
-                    i -= 1
-                i += 1
 
-            self.nextSymbol = self.tokens.nextSymbol
-
-    def parse(self):
-        regEx = re.compile(self.tokens.line)
+    def getTokens(self):
+        tokens = dict()
+        regExLine = regex.compile(self.tokens.line)
         if os.path.isfile(self.oname):
             os.remove(self.oname)
+        with open(self.iname, "r") as inputFile:
+            nextline = inputFile.readline()
+            while nextline:
+                tokenMatch = regex.match(regExLine, nextline)
+                if not tokenMatch:
+                    raise Exception("TokenizerError", "unparsable line found:/n" + nextline)
+                if tokenMatch.groups("multilineCommentEnd"):
+                    raise Exception("TokenizerError", "comment end prior to comment start")
 
-        with open(self.oname, "a") as o:
-            for i in self.lns:
-                out = ""
-                match = regEx.match(i)
-                if not match:
-                    raise Exception("Invalid line: \n" + i + "\n" + ";".join(str(ord(j)) for j in i) + "\n" + regEx.pattern)
-                if match.group("A_Instruction"):
-                    out = "0"
-                    if match.group("const"):
-                        out += "{0:015b}".format(int(match.group("const")))
-                    elif match.group("symbol"):
-                        s = match.group("symbol")
-                        if not s in self.tokens.symbolDict:
-                            self.tokens.symbolDict[s] = self.nextSymbol
-                            self.nextSymbol += 1
-                        out += "{0:015b}".format(self.tokens.symbolDict[s])
-                elif match.group("C_Instruction"):
-                    out = "111"
-                    out += self.tokens.compDict[match.group("comp")]
-                    if match.group("dest"):
-                        out += self.tokens.destDict[match.group("dest")]
-                    else:
-                        out += "000"
-                    if match.group("jump"):
-                        out += self.tokens.jumpDict[match.group("jump")]
-                    else:
-                        out += "000"
-                elif match.group("placeHolder"):
-                    raise Exception("Invalid line: \n" + i)
-                else:
-                    raise Exception("Invalid line: \n" + i)
-                o.write(out + "\n")
+                # https://stackoverflow.com/questions/28856238/how-to-get-group-name-of-match-regular-expression-in-python
+                #the below list comprehension creates a list of the form [(keyword, class), (integer, 123), ...] matching
+                # the tokens found in a given line
+                matchlist = [([n for n, v in a.groupdict().items() if v][0], a.group()) for a in
+                              regex.finditer(d.LexicalElement, line)]
 
+                firstToken = matchlist[0]
+                elementType = firstToken[0]
+                elementValue = firstToken[1]
+                if elementType == "keyword":
+                    if elementValue == "class":
+                        parseClass()
+                elif elementType == "symbol":
+                    raise Exception("TokenizerError", "line begins with symbol:/n" + nextline)
+                elif elementType == "integerConstant":
+                    foo = 1
+                elif elementType == "StringConstant":
+                    foo = 1
+                elif elementType == "identifier":
+                    foo = 1
+                nextline = inputFile.readline()
 def main():
     #parser = parse_args(sys.argv[1:])
     fname = sys.argv[1]
